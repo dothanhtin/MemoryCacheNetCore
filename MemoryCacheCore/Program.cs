90b643c44bf313e963ec1f6c25b1988fb6e3a579
Tindt <Tindt@KV2-PM2-1118>
2020-12-04 11:22:20 +0700

    First commit

---
+++ MemoryCacheCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using MemoryCacheLib;
+using System;
+using System.Threading;
+
+namespace MemoryCacheCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MemoryCacheExtension.AddValue("1", "test 1", 60);
+            var val = MemoryCacheExtension.GetValue("1");
+            Console.WriteLine($"Value of cache: {val}");
+            int count = 1;
+            while (count < 70)
+            {
+                count++;
+                Thread.Sleep(1000);
+                Console.WriteLine($"Thread sleep: {count} s");
+            }
+            var val1 = MemoryCacheExtension.GetValue("1");
+            Console.WriteLine($"Value of cache: {val1}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MemoryCacheLib/MemoryCacheExtension.cs
@@ -0,0 +1,51 @@
+﻿//using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace MemoryCacheLib
+{
+    public class MemoryCacheExtension
+    {
+        private static readonly ObjectCache _cache = MemoryCache.Default;
+        private static readonly long defaultExpiredTime = 60;
+        public static void AddValue<T>(string key, T value)
+        {
+            CacheItemPolicy cacheItemPolicy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(defaultExpiredTime)
+            };
+            _cache.Add(key, value, cacheItemPolicy);
+        }
+
+        public static void AddValue<T>(string key, T value,long timeOut)
+        {
+            CacheItemPolicy cacheItemPolicy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(timeOut)
+            };
+            _cache.Add(key, value, cacheItemPolicy);
+        }
+        public static object GetValue(string key)
+        {
+            var result = _cache.Get(key);
+            return result;
+        }
+        public static bool RemoveKey(string key)
+        {
+            try
+            {
+                while (_cache.Get(key) != null)
+                    _cache.Remove(key);
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+        public static bool checkExistKey(string key)
+        {
+            var result = _cache.Get(key);
+            if (result != null) return true;
+            return false;
+        }
+    }
+}
